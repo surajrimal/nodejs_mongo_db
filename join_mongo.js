41f7674bba4e78e0e377d3e24d0978e67ead0b54
Suraj Rimal <surajrimal50@gmail.com>
2024-03-19 21:39:53 -0400

    Update join_mongo.js

--- join_mongo.js
+++ join_mongo.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://suraj:suraj@cluster0.b7pwh.mongodb.net/mydb?retryWrites=true&w=majority";
+var url = "#####";
 
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
